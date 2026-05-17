bc99082f33f36fbdfbb6477dfa2e366c5c5204e8
joelfrom08 <dev.contact@petbyte.dev>
2026-05-18 00:01:57 +0200

    Remove current block tags
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

--- src/main/java/dev/petbyte/betterglass/datagen/ModBlockTagsProvider.java
+++ src/main/java/dev/petbyte/betterglass/datagen/ModBlockTagsProvider.java
@@ -1,10 +1,8 @@
 package dev.petbyte.betterglass.datagen;
 
-import dev.petbyte.betterglass.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.tags.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -15,8 +13,6 @@ public class ModBlockTagsProvider extends FabricTagsProvider.BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider registries) {
-        valueLookupBuilder(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.TEST_BLOCK);
 
-        valueLookupBuilder(BlockTags.NEEDS_DIAMOND_TOOL).add(ModBlocks.TEST_BLOCK);
     }
 }
