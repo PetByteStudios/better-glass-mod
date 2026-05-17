7727ca120ecdfbb90848c7d36590d063b044e046
joelfrom08 <dev.contact@petbyte.dev>
2026-05-18 00:50:03 +0200

    Add Client Initializer to show version in Actionbar and Chat upon joining
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

---
+++ src/main/java/dev/petbyte/betterglass/BetterGlassClient.java
@@ -0,0 +1,20 @@
+package dev.petbyte.betterglass;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+
+public class BetterGlassClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            client.player.sendSystemMessage(Component.literal("Better Glass").withStyle(style -> style.withBold(true)).append(Component.literal(" v" + BetterGlass.MOD_VERSION).withStyle(style -> style.withBold(false).withItalic(true).withColor(ChatFormatting.RED))));
+        });
+
+        // Disable this for non-beta releases.
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            client.player.sendOverlayMessage(Component.literal("Better Glass").withStyle(style -> style.withBold(true)).append(Component.literal(" v" + BetterGlass.MOD_VERSION).withStyle(style -> style.withBold(false).withItalic(true).withColor(ChatFormatting.RED))));
+        });
+    }
+}
