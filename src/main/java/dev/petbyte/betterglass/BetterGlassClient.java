149d6272598d2d9d0628377fab34ec6729d3a634
joelfrom08 <dev.contact@petbyte.dev>
2026-05-18 19:55:38 +0200

    Small code change
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

--- src/main/java/dev/petbyte/betterglass/BetterGlassClient.java
+++ src/main/java/dev/petbyte/betterglass/BetterGlassClient.java
@@ -8,12 +8,9 @@ import net.minecraft.network.chat.Component;
 public class BetterGlassClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // Disable for non-beta releases.
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             client.player.sendSystemMessage(Component.literal("Better Glass").withStyle(style -> style.withBold(true)).append(Component.literal(" v" + BetterGlass.MOD_VERSION).withStyle(style -> style.withBold(false).withItalic(true).withColor(ChatFormatting.RED))));
-        });
-
-        // Disable this for non-beta releases.
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             client.player.sendOverlayMessage(Component.literal("Better Glass").withStyle(style -> style.withBold(true)).append(Component.literal(" v" + BetterGlass.MOD_VERSION).withStyle(style -> style.withBold(false).withItalic(true).withColor(ChatFormatting.RED))));
         });
     }
