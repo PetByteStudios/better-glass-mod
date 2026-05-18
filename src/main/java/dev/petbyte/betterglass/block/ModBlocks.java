b55a7a41d15366b790f30dbbe0018b5df04da033
joelfrom08 <dev.contact@petbyte.dev>
2026-05-18 03:07:20 +0200

    Compact Block Declaration
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

--- src/main/java/dev/petbyte/betterglass/block/ModBlocks.java
+++ src/main/java/dev/petbyte/betterglass/block/ModBlocks.java
@@ -20,197 +20,45 @@ import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import java.util.function.Function;
 
 public class ModBlocks {
-    public static final Block CLEAR_GLASS = registerBlock("clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
+    private static BlockBehaviour.Properties glassProperties() {
+        return BlockBehaviour.Properties.of()
+                .strength(0.3F)
+                .sound(SoundType.GLASS)
+                .instrument(NoteBlockInstrument.HAT)
+                .noOcclusion()
+                .isValidSpawn(Blocks::never)
+                .isRedstoneConductor(Blocks::never)
+                .isSuffocating(Blocks::never)
+                .isViewBlocking(Blocks::never);
+    }
 
-    public static final Block WHITE_COLORED_CLEAR_GLASS = registerBlock("white_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block LIGHT_GRAY_COLORED_CLEAR_GLASS = registerBlock("light_gray_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block GRAY_COLORED_CLEAR_GLASS = registerBlock("gray_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block BLACK_COLORED_CLEAR_GLASS = registerBlock("black_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block BROWN_COLORED_CLEAR_GLASS = registerBlock("brown_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block RED_COLORED_CLEAR_GLASS = registerBlock("red_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block ORANGE_COLORED_CLEAR_GLASS = registerBlock("orange_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block YELLOW_COLORED_CLEAR_GLASS = registerBlock("yellow_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block LIME_COLORED_CLEAR_GLASS = registerBlock("lime_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block GREEN_COLORED_CLEAR_GLASS = registerBlock("green_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block CYAN_COLORED_CLEAR_GLASS = registerBlock("cyan_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block LIGHT_BLUE_COLORED_CLEAR_GLASS = registerBlock("light_blue_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block BLUE_COLORED_CLEAR_GLASS = registerBlock("blue_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block PURPLE_COLORED_CLEAR_GLASS = registerBlock("purple_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block MAGENTA_COLORED_CLEAR_GLASS = registerBlock("magenta_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
-    public static final Block PINK_COLORED_CLEAR_GLASS = registerBlock("pink_colored_clear_glass",
-            properties -> new TransparentBlock(properties
-                    .strength(0.3F)
-                    .sound(SoundType.GLASS)
-                    .instrument(NoteBlockInstrument.HAT)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::never)
-                    .isRedstoneConductor(Blocks::never)
-                    .isSuffocating(Blocks::never)
-                    .isViewBlocking(Blocks::never)
-            ));
+    public static final Block CLEAR_GLASS = registerGlassBlock("clear_glass");
+    public static final Block WHITE_COLORED_CLEAR_GLASS = registerGlassBlock("white_colored_clear_glass");
+    public static final Block LIGHT_GRAY_COLORED_CLEAR_GLASS = registerGlassBlock("light_gray_colored_clear_glass");
+    public static final Block GRAY_COLORED_CLEAR_GLASS = registerGlassBlock("gray_colored_clear_glass");
+    public static final Block BLACK_COLORED_CLEAR_GLASS = registerGlassBlock("black_colored_clear_glass");
+    public static final Block BROWN_COLORED_CLEAR_GLASS = registerGlassBlock("brown_colored_clear_glass");
+    public static final Block RED_COLORED_CLEAR_GLASS = registerGlassBlock("red_colored_clear_glass");
+    public static final Block ORANGE_COLORED_CLEAR_GLASS = registerGlassBlock("orange_colored_clear_glass");
+    public static final Block YELLOW_COLORED_CLEAR_GLASS = registerGlassBlock("yellow_colored_clear_glass");
+    public static final Block LIME_COLORED_CLEAR_GLASS = registerGlassBlock("lime_colored_clear_glass");
+    public static final Block GREEN_COLORED_CLEAR_GLASS = registerGlassBlock("green_colored_clear_glass");
+    public static final Block CYAN_COLORED_CLEAR_GLASS = registerGlassBlock("cyan_colored_clear_glass");
+    public static final Block LIGHT_BLUE_COLORED_CLEAR_GLASS = registerGlassBlock("light_blue_colored_clear_glass");
+    public static final Block BLUE_COLORED_CLEAR_GLASS = registerGlassBlock("blue_colored_clear_glass");
+    public static final Block PURPLE_COLORED_CLEAR_GLASS = registerGlassBlock("purple_colored_clear_glass");
+    public static final Block MAGENTA_COLORED_CLEAR_GLASS = registerGlassBlock("magenta_colored_clear_glass");
+    public static final Block PINK_COLORED_CLEAR_GLASS = registerGlassBlock("pink_colored_clear_glass");
+
 
     private static Block registerBlock(String name, Function<BlockBehaviour.Properties, Block> function) {
         Block toRegister = function.apply(BlockBehaviour.Properties.of().setId(ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(BetterGlass.MOD_ID, name))));
+        registerBlockItem(name, toRegister);
+        return Registry.register(BuiltInRegistries.BLOCK, Identifier.fromNamespaceAndPath(BetterGlass.MOD_ID, name), toRegister);
+    }
+
+    private static Block registerGlassBlock(String name) {
+        Block toRegister = new TransparentBlock(glassProperties().setId(ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(BetterGlass.MOD_ID, name))));
         registerBlockItem(name, toRegister);
         return Registry.register(BuiltInRegistries.BLOCK, Identifier.fromNamespaceAndPath(BetterGlass.MOD_ID, name), toRegister);
     }
