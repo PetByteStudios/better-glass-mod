3a5860736deaed2065f65a6cf7ca862508eb581d
joelfrom08 <dev.contact@petbyte.dev>
2026-05-23 04:32:50 +0200

    Add Built-In Resource Pack
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

--- src/main/java/dev/petbyte/betterglass/BetterGlass.java
+++ src/main/java/dev/petbyte/betterglass/BetterGlass.java
@@ -6,7 +6,11 @@ import dev.petbyte.betterglass.item.ModItems;
 import dev.petbyte.betterglass.tab.ModCreativeModeTabs;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,5 +31,12 @@ public class BetterGlass implements ModInitializer {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModMenus.RegisterGlasscutterMenu();
+
+		ResourceManagerHelper.registerBuiltinResourcePack(
+				Identifier.fromNamespaceAndPath("betterglass", "forced_resources"),
+				FabricLoader.getInstance().getModContainer("betterglass").orElseThrow(),
+				Component.literal("Better Glass Resources"),
+				ResourcePackActivationType.ALWAYS_ENABLED
+		);
 	}
 }
