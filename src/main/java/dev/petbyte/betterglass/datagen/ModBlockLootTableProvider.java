c4a96edc58a205b58074fbdc83cba9b21ea4e336
joelfrom08 <dev.contact@petbyte.dev>
2026-06-07 01:00:53 +0200

    Fix Tinted loot tables
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

--- src/main/java/dev/petbyte/betterglass/datagen/ModBlockLootTableProvider.java
+++ src/main/java/dev/petbyte/betterglass/datagen/ModBlockLootTableProvider.java
@@ -16,9 +16,33 @@ public class ModBlockLootTableProvider extends FabricBlockLootSubProvider {
     @Override
     public void generate() {
         for (Block block : ModBlocks.BETTER_GLASS_ALL) {
-            add(block, createSilkTouchOnlyTable(block));
+            if (isTinted(block)) {
+                dropSelf(block);
+            } else {
+                add(block, createSilkTouchOnlyTable(block));
+            }
         }
 
         dropSelf(ModBlocks.GLASSCUTTER);
+    }
+
+    private boolean isTinted(Block block) {
+        return ModBlocks.CLEAR_TINTED_GLASS == block
+                || ModBlocks.SCRATCHED_TINTED_GLASS == block
+                || ModBlocks.CLEAR_TINTED_GLASS_PANE == block
+                || ModBlocks.SCRATCHED_TINTED_GLASS_PANE == block
+                || ModBlocks.COLORED_TINTED_GLASS.containsValue(block)
+                || ModBlocks.COLORED_TINTED_GLASS_PANE.containsValue(block)
+                || ModBlocks.STAINED_TINTED_GLASS.containsValue(block)
+                || ModBlocks.STAINED_TINTED_GLASS_PANE.containsValue(block)
+                || ModBlocks.COLORED_CLEAR_TINTED_GLASS.containsValue(block)
+                || ModBlocks.COLORED_CLEAR_TINTED_GLASS_PANE.containsValue(block)
+                || ModBlocks.STAINED_CLEAR_TINTED_GLASS.containsValue(block)
+                || ModBlocks.STAINED_CLEAR_TINTED_GLASS_PANE.containsValue(block)
+                || ModBlocks.COLORED_SCRATCHED_TINTED_GLASS.containsValue(block)
+                || ModBlocks.COLORED_SCRATCHED_TINTED_GLASS_PANE.containsValue(block)
+                || ModBlocks.STAINED_SCRATCHED_TINTED_GLASS.containsValue(block)
+                || ModBlocks.STAINED_SCRATCHED_TINTED_GLASS_PANE.containsValue(block)
+                || ModBlocks.TINTED_GLASS_PANE == block;
     }
 }
