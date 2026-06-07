e362886a60b2ef83cc0ac16da2aac1f65641cc9f
joelfrom08 <dev.contact@petbyte.dev>
2026-06-07 02:26:34 +0200

    Remove System and Overlay messages upon joining
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

--- src/main/java/dev/petbyte/betterglass/BetterGlassClient.java
+++ src/main/java/dev/petbyte/betterglass/BetterGlassClient.java
@@ -2,24 +2,15 @@ package dev.petbyte.betterglass;
 
 import dev.petbyte.betterglass.glasscutter.ModMenus;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 
 public class BetterGlassClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Disable for non-beta releases.
-        ClientPlayConnectionEvents.JOIN.register((_, _, client) -> {
-            assert client.player != null;
-            client.player.sendSystemMessage(Component.literal("Better Glass").withStyle(style -> style.withBold(true)).append(Component.literal(" v" + BetterGlass.MOD_VERSION).withStyle(style -> style.withBold(false).withItalic(true).withColor(ChatFormatting.RED))));
-            client.player.sendOverlayMessage(Component.literal("Better Glass").withStyle(style -> style.withBold(true)).append(Component.literal(" v" + BetterGlass.MOD_VERSION).withStyle(style -> style.withBold(false).withItalic(true).withColor(ChatFormatting.RED))));
-        });
-
         ResourceLoader.registerBuiltinPack(
                 Identifier.fromNamespaceAndPath(BetterGlass.MOD_ID, "connecting_textures"),
                 FabricLoader.getInstance().getModContainer(BetterGlass.MOD_ID).orElseThrow(),
