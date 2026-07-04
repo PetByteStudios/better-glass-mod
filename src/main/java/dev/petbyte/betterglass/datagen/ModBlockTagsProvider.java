b469eaa3922b8ef0de52b6b37cff018281763dab
joelfrom08 <dev.contact@petbyte.dev>
2026-07-04 14:05:42 +0200

    Add more specific Patterned Glass tags
    Signed-off-by: joelfrom08 <dev.contact@petbyte.dev>

--- src/main/java/dev/petbyte/betterglass/datagen/ModBlockTagsProvider.java
+++ src/main/java/dev/petbyte/betterglass/datagen/ModBlockTagsProvider.java
@@ -290,22 +290,54 @@ public class ModBlockTagsProvider extends FabricTagsProvider.BlockTagsProvider {
 
 
         for (String motif : ModBlocks.PATTERN_MOTIFS) {
-            valueLookupBuilder(ModTags.ANY_PATTERNED_GLASS_BLOCK.block())
-                    .add(ModBlocks.PATTERNED_GLASS_BLOCKS.get(motif))
-                    .add(ModBlocks.PATTERNED_CLEAR_GLASS_BLOCKS.get(motif))
-                    .add(ModBlocks.PATTERNED_SCRATCHED_GLASS_BLOCKS.get(motif))
-                    .add(ModBlocks.PATTERNED_TINTED_GLASS_BLOCKS.get(motif))
-                    .add(ModBlocks.PATTERNED_CLEAR_TINTED_GLASS_BLOCKS.get(motif))
-                    .add(ModBlocks.PATTERNED_SCRATCHED_TINTED_GLASS_BLOCKS.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_GLASS_BLOCK.block()).add(ModBlocks.PATTERNED_GLASS_BLOCKS.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_CLEAR_GLASS_BLOCK.block()).add(ModBlocks.PATTERNED_CLEAR_GLASS_BLOCKS.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_SCRATCHED_GLASS_BLOCK.block()).add(ModBlocks.PATTERNED_SCRATCHED_GLASS_BLOCKS.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_TINTED_GLASS_BLOCK.block()).add(ModBlocks.PATTERNED_TINTED_GLASS_BLOCKS.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_CLEAR_TINTED_GLASS_BLOCK.block()).add(ModBlocks.PATTERNED_CLEAR_TINTED_GLASS_BLOCKS.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_SCRATCHED_TINTED_GLASS_BLOCK.block()).add(ModBlocks.PATTERNED_SCRATCHED_TINTED_GLASS_BLOCKS.get(motif));
 
-            valueLookupBuilder(ModTags.ANY_PATTERNED_GLASS_PANE.block())
-                    .add(ModBlocks.PATTERNED_GLASS_PANES.get(motif))
-                    .add(ModBlocks.PATTERNED_CLEAR_GLASS_PANES.get(motif))
-                    .add(ModBlocks.PATTERNED_SCRATCHED_GLASS_PANES.get(motif))
-                    .add(ModBlocks.PATTERNED_TINTED_GLASS_PANES.get(motif))
-                    .add(ModBlocks.PATTERNED_CLEAR_TINTED_GLASS_PANES.get(motif))
-                    .add(ModBlocks.PATTERNED_SCRATCHED_TINTED_GLASS_PANES.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_GLASS_PANE.block()).add(ModBlocks.PATTERNED_GLASS_PANES.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_CLEAR_GLASS_PANE.block()).add(ModBlocks.PATTERNED_CLEAR_GLASS_PANES.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_SCRATCHED_GLASS_PANE.block()).add(ModBlocks.PATTERNED_SCRATCHED_GLASS_PANES.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_TINTED_GLASS_PANE.block()).add(ModBlocks.PATTERNED_TINTED_GLASS_PANES.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_CLEAR_TINTED_GLASS_PANE.block()).add(ModBlocks.PATTERNED_CLEAR_TINTED_GLASS_PANES.get(motif));
+            valueLookupBuilder(ModTags.PATTERNED_SCRATCHED_TINTED_GLASS_PANE.block()).add(ModBlocks.PATTERNED_SCRATCHED_TINTED_GLASS_PANES.get(motif));
         }
+        valueLookupBuilder(ModTags.PATTERNED_GLASS_ALL.block())
+                .addTag(ModTags.PATTERNED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_GLASS_PANE.block());
+        valueLookupBuilder(ModTags.PATTERNED_CLEAR_GLASS_ALL.block())
+                .addTag(ModTags.PATTERNED_CLEAR_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_CLEAR_GLASS_PANE.block());
+        valueLookupBuilder(ModTags.PATTERNED_SCRATCHED_GLASS_ALL.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_GLASS_PANE.block());
+        valueLookupBuilder(ModTags.PATTERNED_TINTED_GLASS_ALL.block())
+                .addTag(ModTags.PATTERNED_TINTED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_TINTED_GLASS_PANE.block());
+        valueLookupBuilder(ModTags.PATTERNED_CLEAR_TINTED_GLASS_ALL.block())
+                .addTag(ModTags.PATTERNED_CLEAR_TINTED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_CLEAR_TINTED_GLASS_PANE.block());
+        valueLookupBuilder(ModTags.PATTERNED_SCRATCHED_TINTED_GLASS_ALL.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_TINTED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_TINTED_GLASS_PANE.block());
+
+        valueLookupBuilder(ModTags.ANY_PATTERNED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_CLEAR_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_TINTED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_CLEAR_TINTED_GLASS_BLOCK.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_TINTED_GLASS_BLOCK.block());
+        valueLookupBuilder(ModTags.ANY_PATTERNED_GLASS_PANE.block())
+                .addTag(ModTags.PATTERNED_GLASS_PANE.block())
+                .addTag(ModTags.PATTERNED_CLEAR_GLASS_PANE.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_GLASS_PANE.block())
+                .addTag(ModTags.PATTERNED_TINTED_GLASS_PANE.block())
+                .addTag(ModTags.PATTERNED_CLEAR_TINTED_GLASS_PANE.block())
+                .addTag(ModTags.PATTERNED_SCRATCHED_TINTED_GLASS_PANE.block());
+
         valueLookupBuilder(ModTags.ANY_PATTERNED_GLASS_ALL.block())
                 .addTag(ModTags.ANY_PATTERNED_GLASS_BLOCK.block())
                 .addTag(ModTags.ANY_PATTERNED_GLASS_PANE.block());
